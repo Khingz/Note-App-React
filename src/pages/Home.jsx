866ffe36c514c2171fcb9340c7af39fe09b6f8c6
khingz <kingsley995mr@gmail.com>
2022-12-18 21:29:36 +0100

    added home page and style files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import homeImg from "../assets/backgrounds/landing-bgg.png";
+import "../styles/Home.css";
+import { TfiLayoutLineSolid } from "react-icons/tfi";
+import { AiOutlineSwapRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home__container">
+      <div className="home__left">
+        <img src={homeImg} alt="home" />
+      </div>
+      <div className="home__right">
+        <p>
+          Make all <span>your</span> <span>things</span> <span>organized</span>
+        </p>
+        <div className="home__login__signup">
+          <div className="home__signup">
+            <Link to="/signin">
+              <TfiLayoutLineSolid />
+              SignUp
+            </Link>
+          </div>
+          <div className="home__login">
+            <Link to="/signin">
+              Signin
+              <AiOutlineSwapRight />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
