0c6081861bc341c6fdba4733f797b2c8cf128b9c
khingz <kingsley995mr@gmail.com>
2023-01-07 23:20:45 +0100

    post api call to firebase to add note

--- src/pages/AddNote.jsx
+++ src/pages/AddNote.jsx
@@ -1,26 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../component/navbar";
 import "../styles/AddNote.css";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const AddNote = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [category, setCategory] = useState("personal");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!title || !message || !category) {
+      console.log('error');
+      return false;
+    }
+    createNote()
+    // navigate('/dashboard');
+  };
+
+  const createNote = async () => {
+    const notesCollectionRef = collection(db, "notes");
+    const data = {
+      title, message, category
+    }
+    await addDoc(notesCollectionRef, data)
+  }
+
   return (
     <div className="add__note__main">
       <Navbar />
       <div className="add__note__container">
         <h3>Add New Note</h3>
-        <form action="">
+        <form action=""  onSubmit={handleSubmit}>
           <div className="note__title">
             <label htmlFor="">Note title</label>
-            <input type="text" name="title" id="title" />
+            <input
+              type="text"
+              name="title"
+              id="title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
           <div className="note__content">
             <label htmlFor="">Note Content</label>
-            <textarea name="note" id="note" cols="30" rows="10"></textarea>
+            <textarea
+              name="note"
+              id="note"
+              cols="30"
+              rows="10"
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
           </div>
           <div className="add__category">
             <label htmlFor="category">Category</label>
-            <select name="category" id="category">
+            <select
+              name="category"
+              id="category"
+              onChange={(e) => setCategory(e.target.value)}
+            >
               <option value="personal">Personal</option>
               <option value="religious">Religious</option>
               <option value="business">Business</option>
