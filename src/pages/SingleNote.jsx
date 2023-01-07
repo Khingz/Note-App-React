893e95b067bf8017db7797e2998e053362e145bc
Khingz <kingsley995mr@gmail.com>
2023-01-07 17:26:50 +0100

    adding single page content

--- src/pages/SingleNote.jsx
+++ src/pages/SingleNote.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { NoteContext } from '../context/Context';
 import Navbar from '../component/navbar';
 
+import '../styles/SingleNote.css'
+
 const SingleNote = () => {
   const { noteId } = useParams();
   const { state } = NoteContext();
@@ -37,8 +39,13 @@ const SingleNote = () => {
   return (
     <div>
       <Navbar />
-      <h2>{note.title}</h2>
-      <p>{note.message}</p>
+      <div className="top__line"></div>
+        <div className="single__note__container">
+          <h4 className='single__note__title'>{note.title}</h4>
+          <p className="single__note__message">{note.message}</p>
+        </div>
+
+      <div className="bottom__line"></div>
     </div>
   );
 };
